b8e86cb5125878f13c71bd0b92842fe1ce3c734c
mladicstefan6 <mladicstefan6@gmail.com>
2025-06-24 16:55:42 +0200

    remove arrow keys

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,6 +2,12 @@
 
 local M = {}
 
+--Disable Arrow keys in normal mode
+vim.api.nvim_set_keymap('n','<Up>'   , '<Nop>', {noremap = true, silent= true})
+vim.api.nvim_set_keymap('n','<Down>' , '<Nop>', {noremap = true, silent= true})
+vim.api.nvim_set_keymap('n','<Left>' , '<Nop>', {noremap = true, silent= true})
+vim.api.nvim_set_keymap('n','<Right>', '<Nop>', {noremap = true, silent= true})
+
 M.general = {
   i = {
     -- go to  beginning and end
