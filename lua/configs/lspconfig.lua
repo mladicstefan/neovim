3074e7676b94d34c791ab949745050c5fecba05d
mladicstefan <mladicstefan6@gmail.com>
2025-07-21 23:54:19 +0200

    i love spamming commits

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local lspconfig = require "lspconfig"
+
+local nvlsp = require "nvchad.configs.lspconfig"
+
 local servers = { "rust", "cssls" }
 vim.lsp.enable(servers)
-
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
 -- read :h vim.lsp.config for changing options of lsp servers 
