d95a145060d072c8594d852fee6224e6a06dfa2e
mladicstefan <mladicstefan6@gmail.com>
2025-09-16 20:52:19 +0200

    python support

--- init.lua
+++ init.lua
@@ -38,6 +38,22 @@ lspconfig.rust_analyzer.setup({
     },
 })
 
+lspconfig.basedpyright.setup({
+  capabilities = capabilities,
+  settings = {
+    python = {
+      pythonPath = ".venv/bin/python",
+    },
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true,
+      }
+    },
+  },
+})
+
 lspconfig.lua_ls.setup({
   capabilities = capabilities,
   settings = {
@@ -60,7 +76,9 @@ lspconfig.lua_ls.setup({
 })
 
 require('nvim-treesitter.configs').setup({
- ensure_installed = { "c", "cpp","rust", "lua", "vim", "vimdoc", "query" },
+ ensure_installed = { "c", "cpp","rust","python","lua", "vim", "vimdoc", "query" },
+ modules = {},
+ ignore_install = {},
  sync_install = false,
  auto_install = true,
  highlight = {
