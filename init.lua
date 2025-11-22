8f169ff3e8d1ff8516e5c5a4fa21622027af92d3
mladicstefan <mladicstefan6@gmail.com>
2025-11-22 03:05:05 +0100

    dodaj kamilu

--- init.lua
+++ init.lua
@@ -1,89 +1,98 @@
 -- vim.pack.add (requires Neovim 0.12+)
 vim.pack.add({
- { src = 'https://github.com/nvim-lua/plenary.nvim' },
- { src = 'https://github.com/nvim-telescope/telescope.nvim' },
- { src = 'https://github.com/neovim/nvim-lspconfig' },
- { src = 'https://github.com/saghen/blink.cmp' },
- { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
- { src = 'https://github.com/windwp/nvim-autopairs' },
- { src = 'https://github.com/chomosuke/typst-preview.nvim' },
- { src = 'https://github.com/stevearc/conform.nvim' },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/saghen/blink.cmp" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/windwp/nvim-autopairs" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
 })
-
 -- vim.cmd.colorscheme('habamax')
 --ubuntu colors
-vim.cmd.colorscheme('ubuntu')
-
-require('options')
-require('mappings')
-require('autocmds')
-require('lsp')
-
-require('nvim-treesitter.configs').setup({
- ensure_installed = {
-  "c", "cpp", "rust", "python", "go", "lua", "vim", "vimdoc", "query",
-  "svelte", "javascript", "typescript", "html", "css","zig","typst"
- },
- modules = {},
- ignore_install = {},
- sync_install = false,
- auto_install = true,
- highlight = {
-   enable = true,
-   additional_vim_regex_highlighting = false,
- },
- indent = {
-   enable = true
- },
+vim.cmd.colorscheme("ubuntu")
+require("options")
+require("mappings")
+require("autocmds")
+require("lsp")
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"c",
+		"cpp",
+		"rust",
+		"python",
+		"go",
+		"lua",
+		"vim",
+		"vimdoc",
+		"query",
+		"svelte",
+		"javascript",
+		"typescript",
+		"html",
+		"css",
+		"zig",
+		"typst",
+		"ocaml",
+		"ocaml_interface",
+	},
+	modules = {},
+	ignore_install = {},
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
 })
-
-local autopairs = require('nvim-autopairs')
+local autopairs = require("nvim-autopairs")
 autopairs.setup({})
-
-require('blink.cmp').setup({
-  keymap = {
-    preset = 'enter',
-    ['<Tab>'] = { 'select_next', 'fallback' },
-    ['<S-Tab>'] = { 'select_prev', 'fallback' },
-  },
-  appearance = {
-    use_nvim_cmp_as_default = true,
-  },
-  sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer' },
-  },
+require("blink.cmp").setup({
+	keymap = {
+		preset = "enter",
+		["<Tab>"] = { "select_next", "fallback" },
+		["<S-Tab>"] = { "select_prev", "fallback" },
+	},
+	appearance = {
+		use_nvim_cmp_as_default = true,
+	},
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
 })
-
-local telescope = require('telescope')
+local telescope = require("telescope")
 telescope.setup({
- defaults = {
-   file_ignore_patterns = { "%.o", "%.so", "%.a" },
- }
+	defaults = {
+		file_ignore_patterns = { "%.o", "%.so", "%.a" },
+	},
 })
-
-require('typst-preview').setup({})
-
+require("typst-preview").setup({})
 --formatting
-require('conform').setup({
-  formatters_by_ft = {
-    c = { 'clang_format' },
-    cpp = { 'clang_format' },
-    rust = { 'rustfmt' },
-    python = { 'isort', 'black' },
-    go = { 'goimports', 'gofmt' },
-    lua = { 'stylua' },
-    javascript = { 'prettier' },
-    typescript = { 'prettier' },
-    svelte = { 'prettier' },
-    html = { 'prettier' },
-    css = { 'prettier' },
-    bash = { 'shfmt' },
-    sh = { 'shfmt' },
-    zig = { 'zigfmt' },
-  },
-  -- Format on save
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = 'fallback',
-  },
+require("conform").setup({
+	formatters_by_ft = {
+		c = { "clang_format" },
+		cpp = { "clang_format" },
+		rust = { "rustfmt" },
+		python = { "isort", "black" },
+		go = { "goimports", "gofmt" },
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		svelte = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
+		bash = { "shfmt" },
+		sh = { "shfmt" },
+		zig = { "zigfmt" },
+		ocaml = { "ocamlformat" },
+	},
+	-- Format on save
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,162 +1,153 @@
-local lspconfig = require('lspconfig')
-local capabilities = require('blink.cmp').get_lsp_capabilities()
-
+local lspconfig = require("lspconfig")
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 -- C/C++
 lspconfig.clangd.setup({
- capabilities = capabilities,
- cmd = {
-   'clangd',
-   '--background-index',
-   '--clang-tidy=false',
-   '--completion-style=bundled',
-   '--header-insertion=never',
- },
- filetypes = { 'c', 'cpp' },
+	capabilities = capabilities,
+	cmd = {
+		"clangd",
+		"--background-index",
+		"--clang-tidy=false",
+		"--completion-style=bundled",
+		"--header-insertion=never",
+	},
+	filetypes = { "c", "cpp" },
 })
-
 -- Rust
 lspconfig.rust_analyzer.setup({
-    capabilities = capabilities,
-    settings = {
-        ['rust-analyzer'] = {
-            checkOnSave = true,
-        }
-    },
+	capabilities = capabilities,
+	settings = {
+		["rust-analyzer"] = {
+			checkOnSave = true,
+		},
+	},
 })
-
 -- Lua
 lspconfig.lua_ls.setup({
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
-
 -- Python
 lspconfig.basedpyright.setup({
-  capabilities = capabilities,
-  settings = {
-    python = {
-        pythonPath = vim.fn.exepath('python'),
-    },
-    basedpyright = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "openFilesOnly",
-        useLibraryCodeForTypes = true,
-      }
-    },
-  },
+	capabilities = capabilities,
+	settings = {
+		python = {
+			pythonPath = vim.fn.exepath("python"),
+		},
+		basedpyright = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 })
-
 -- Bash
 lspconfig.bashls.setup({
-  capabilities = capabilities,
-  filetypes = { 'sh', 'bash' },
+	capabilities = capabilities,
+	filetypes = { "sh", "bash" },
 })
-
 -- Golang
 lspconfig.gopls.setup({
-  capabilities = capabilities,
-  settings = {
-    gopls = {
-      gofumpt = true,
-      codelenses = {
-        gc_details = false,
-        generate = true,
-        regenerate_cgo = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-        vendor = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        nilness = true,
-        unusedparams = true,
-        unusedwrite = true,
-        useany = true,
-      },
-      usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-      semanticTokens = true,
-    },
-  },
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			gofumpt = true,
+			codelenses = {
+				gc_details = false,
+				generate = true,
+				regenerate_cgo = true,
+				run_govulncheck = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = true,
+			},
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+			analyses = {
+				nilness = true,
+				unusedparams = true,
+				unusedwrite = true,
+				useany = true,
+			},
+			usePlaceholders = true,
+			completeUnimported = true,
+			staticcheck = true,
+			directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+			semanticTokens = true,
+		},
+	},
 })
-
 -- Svelte
 lspconfig.svelte.setup({
-  capabilities = capabilities,
-  filetypes = { 'svelte' },
+	capabilities = capabilities,
+	filetypes = { "svelte" },
 })
-
 -- Typescript
 lspconfig.ts_ls.setup({
-  capabilities = capabilities,
-})lspconfig.ts_ls.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
-
 -- Tailwind CSS
 lspconfig.tailwindcss.setup({
-  capabilities = capabilities,
-  filetypes = { 'html', 'css', 'svelte', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+	capabilities = capabilities,
+	filetypes = { "html", "css", "svelte", "javascript", "typescript", "javascriptreact", "typescriptreact" },
 })
-
 --Zig
 lspconfig.zls.setup({
-  capabilities = capabilities,
+	capabilities = capabilities,
 })
-
+-- OCaml
+lspconfig.ocamllsp.setup({
+	capabilities = capabilities,
+	filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+})
 -- LSP keybinds
-vim.api.nvim_create_autocmd('LspAttach', {
- group = vim.api.nvim_create_augroup('UserLspConfig', {}),
- callback = function(ev)
-   local opts = { buffer = ev.buf }
-   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-   -- Add manual format keymap (optional, since you have format_on_save)
-   vim.keymap.set({'n', 'v'}, '<leader>fm', function()
-     require('conform').format({ bufnr = ev.buf, lsp_format = 'fallback' })
-   end, opts)
- end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		-- Add manual format keymap (optional, since you have format_on_save)
+		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+			require("conform").format({ bufnr = ev.buf, lsp_format = "fallback" })
+		end, opts)
+	end,
 })
-
 --Typst
 lspconfig.tinymist.setup({
-  capabilities = capabilities,
-  settings = {
-    formatterMode = "typstyle",
-        exportPdf = "onSave",
-    },
+	capabilities = capabilities,
+	settings = {
+		formatterMode = "typstyle",
+		exportPdf = "onSave",
+	},
 })
